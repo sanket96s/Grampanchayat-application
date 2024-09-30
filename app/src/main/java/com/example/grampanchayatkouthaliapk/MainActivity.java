730cffe3bfd22dd92574d2b75ef74af328a523bb
sanket96s <sanketm9797@gmail.com>
2024-09-30 16:34:59 +0530

    nt

--- app/src/main/java/com/example/grampanchayatkouthaliapk/MainActivity.java
+++ app/src/main/java/com/example/grampanchayatkouthaliapk/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.grampanchayatkouthaliapk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView iconNotification = findViewById(R.id.icon_notification);
+        iconNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirect to NotificationActivity
+                Intent intent = new Intent(MainActivity.this, NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
